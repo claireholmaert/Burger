a30fc4fc360a2266b38f098e8cf98c37a8e3c986
miliweboss <claireholmaert@gmail.com>
2024-04-16 13:59:56 +0200

    responsive: lastProducts

--- burger/src/components/marketing/sections/LastProducts.jsx
+++ burger/src/components/marketing/sections/LastProducts.jsx
@@ -9,22 +9,22 @@ import Image3 from '../../../../public/medias/images/burger-veggie.jpeg';
 function LastProducts() {
   return (
         <Container>
-            <div className="grid grid-cols-2 grid-rows-2 gap-9 w-full h-96 px-11 my-20 text-white uppercase tracking-wider">
-                <div className="relative row-span-2 bg-yellow h-full rounded-md p-7">
+            <div className="grid lg:grid-cols-2 lg:grid-rows-2 gap-3 lg:gap-9 w-full h-96 px-11 my-20 text-white uppercase tracking-wider">
+                <div className="relative lg:row-span-2 bg-yellow h-full rounded-md p-5 lg:p-7">
                     <img src={Image1} className="absolute top-0 left-0 w-full h-full object-cover z-0 rounded-md" alt="Un délicieux hamburger exposé sur une pièce de bois" />
                     <div className="relative">
                     <p className="font-bold">L'Explosion Tex-Mex</p>
                     <p className="text-2xl">Savoureux & relevé</p>
                     </div>
                 </div>
-                <div className="relative bg-yellow h-full rounded-md p-7">
+                <div className="relative bg-yellow h-full rounded-md p-5 lg:p-7">
                     <img src={Image2} className="absolute top-0 left-0 w-full h-full object-cover z-0 rounded-md" alt="Un hamburger vu du dessus avec des frites dans un recipient en forme de bateau et des anneaux d'oignons rouge à côté" />
                     <div className="relative">
                     <p className="font-bold">Le Suprême Saumoné</p>
                     <p className="text-2xl">Frais & délicat</p>
                     </div>
                 </div>
-                <div className="relative bg-yellow h-full rounded-md p-7">
+                <div className="relative bg-yellow h-full rounded-md p-5 lg:p-7">
                     <img src={Image3} className="absolute top-0 left-0 w-full h-full object-cover z-0 rounded-md" alt="Un hamburger veggie" />
                     <div className="relative">
                     <p className="font-bold">L'Extravagant Veggie</p>
